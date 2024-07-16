5d4db30d35bcc5b77bcef230451bc712ca96c055
franklinmartin <84481627+franklinmartin@users.noreply.github.com>
2024-07-15 21:32:13 -0400

    update site

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,9 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "franklinmartin.dev",
   EMAIL: "franklinmartin6138@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_WORKS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 5,
 };
 
 export const HOME: Metadata = {
@@ -29,10 +29,10 @@ export const PROJECTS: Metadata = {
 };
 
 export const SOCIALS: Socials = [
-  { 
+/*   { 
     NAME: "twitter",
     HREF: "https://twitter.com/",
-  },
+  }, */
   { 
     NAME: "github",
     HREF: "https://github.com/franklinmartin/"
